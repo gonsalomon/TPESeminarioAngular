b3fb2987439bd9fa1423ef020ee132bf079b9457
Gonzalo Salomon <salomongonzalo8@gmail.com>
2022-11-07 20:24:53 -0300

    added components

--- boomer-jokes/src/app/app.module.ts
+++ boomer-jokes/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RandomComponent } from './random/random.component';
+import { WordComponent } from './word/word.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomComponent,
+    WordComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
